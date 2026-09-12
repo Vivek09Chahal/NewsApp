0b39fbc9d8f634c8c8617dd08d29df0892b30d8a
Vivek <vivekchahal1156@gmail.com>
2026-09-12 15:40:56 +0530

    Initial Commit

---
+++ NewsApp/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  NewsApp
+//
+//  Created by Vivek on 12/09/26.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
